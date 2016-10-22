ca35bf3f85abd4d1ff745b4d5d1144e2a3525d94
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-10-22 11:23:55 +0100

    Adding Note Model

---
+++ src/Models/Note.php
@@ -0,0 +1,92 @@
+<?php namespace Arcanedev\LaravelNotes\Models;
+
+use Arcanedev\LaravelNotes\Bases\Model;
+
+/**
+ * Class     Note
+ *
+ * @package  Arcanedev\LaravelNotes\Models
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ *
+ * @property  int                                  id
+ * @property  string                               content
+ * @property  int                                  noteable_id
+ * @property  string                               noteable_type
+ * @property  int                                  author_id
+ * @property  \Illuminate\Database\Eloquent\Model  author
+ * @property  \Carbon\Carbon                       created_at
+ * @property  \Carbon\Carbon                       updated_at
+ */
+class Note extends Model
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['content', 'author_id'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['noteable_id', 'noteable_type'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id'          => 'integer',
+        'noteable_id' => 'integer',
+        'author_id'   => 'integer',
+    ];
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Note constructor.
+     *
+     * @param  array  $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->setTable(
+            $this->getTableFromConfig('notes', 'notes')
+        );
+
+        parent::__construct($attributes);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Relationship
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The noteable relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function noteable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * The author relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo($this->getModelFromConfig('authors'));
+    }
+}
