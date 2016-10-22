bdbdebebf08be06b1e4c36eddeff92470d14fa5e
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-10-22 11:23:26 +0100

    Adding the service provider

---
+++ src/LaravelNotesServiceProvider.php
@@ -0,0 +1,69 @@
+<?php namespace Arcanedev\LaravelNotes;
+
+use Arcanedev\Support\PackageServiceProvider;
+
+/**
+ * Class     LaravelNotesServiceProvider
+ *
+ * @package  Arcanedev\LaravelNotes
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class LaravelNotesServiceProvider extends PackageServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Package name.
+     *
+     * @var string
+     */
+    protected $package = 'laravel-notes';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the base path of the package.
+     *
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return dirname(__DIR__);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->registerConfig();
+    }
+
+    /**
+     * Boot the service provider.
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->publishMigrations();
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
